616d817d57770fb9e8d9c031edbb47fdbf20fcdc
hatimbijjou <hatim.bijjou@gmail.com>
2024-04-01 01:42:47 +0000

    postgres database connection setup

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const pgp = require('pg-promise')()
+const db = pgp('postgres://macdehatim:admin@localhost:5432/invoicerdb')
 
-app.get('/', (req, res) => {
-  res.send('Hatim is cooking the backend')
-})
+
+
+app.get('/', async (req, res) => {
+  try {
+    let weather = await db.any('SELECT * FROM weather');
+    res.send(JSON.stringify(weather));
+  } catch (error) {
+    console.log('ERROR:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Backend is listening on port ${port}`)
